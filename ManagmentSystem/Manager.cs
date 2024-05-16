e9c853fba2adc3a8fdd67711342bbf025a9d86eb
Ha Khanh Nguyen | 4SELLERS <ha-khanh.nguyen@4sellers.de>
2024-05-16 18:55:18 +0200

    feat: new function@

--- ManagmentSystem/Manager.cs
+++ ManagmentSystem/Manager.cs
@@ -12,8 +12,8 @@
 
         public void PrintMenu()
         {
-            Console.WriteLine("Welcome to my managment system!" + Environment.NewLine);
-            Console.WriteLine("1. Print all uers");
+            Console.WriteLine("Welcome to my management system!" + Environment.NewLine);
+            Console.WriteLine("1. Print all user");
             Console.WriteLine("2. Add user");
             Console.WriteLine("3. Edit user");
             Console.WriteLine("4. Search user");
@@ -22,12 +22,52 @@
 
             Console.Write("Enter your menu option: ");
 
+            bool tryParse = int.TryParse(Console.ReadLine(), out int menuOption);
 
-            int menuOption = Convert.ToInt32(Console.ReadLine());
+            if (!tryParse)
+            {
+                Console.WriteLine(menuOption);
 
+                if (menuOption == 1)
+                {
+                    PrintAll();
+                } else if (menuOption == 2)
+                {
+                    AddPerson();
+                } else if (menuOption == 3)
+                {
+                    EditPerson();
+                } else if (menuOption == 4)
+                {
+                    SearchPerson();
+                } else if (menuOption == 5)
+                {
+                    RemovePerson();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect menu choice.");
+                Console.WriteLine("Press <Enter> to try again.");
+                Console.ReadLine();
+                Console.Clear();
+                PrintMenu();
+            }
           
             Console.WriteLine(menuOption);
         }
+
+        public void PrintAll()
+        {
+            people.ForEach(delegate (Person person)
+            {
+                Console.WriteLine(person.ReturnDetails());
+            });
+        }
+        public void AddPerson() {}
+        public void EditPerson() {}
+        public void SearchPerson() {}
+        public void RemovePerson() {}
 
     }
 }
